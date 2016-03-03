e319d13a936acb62ca6a9ca8a428df4f368e5686
stephan-botha <fanusbotha@yahoo.com>
2016-03-02 19:37:31 -0800

    Assets update
    Characters, buildings, basic animations and scripts

---
+++ SevenWeapons/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float aggroRange = 20f;
+    public float attackRange = 3f;
+    public float patrolIdleTime = 4f;
+    public float fleeHealth = 50f;
+    
+    public Transform[] patrolPoints;
+
+    int currentPatrolPoint;
+
+    Transform player;
+    Vector3 fleeTarget;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    NavMeshAgent nav;
+    EnemyAttack enemyAttack;
+    Animator anim;
+
+    float patrolIdleTimer = 0f;
+    float fleeDistance;
+    bool hasFled = false;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
+        enemyAttack = GetComponent<EnemyAttack>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        currentPatrolPoint = 0;
+        fleeDistance = 10f;
+        fleeTarget.Set (0f,0f,0f);
+        nav.enabled = true;
+    }
+
+    void Update()
+    {
+        if (enemyHealth.currentHealth <= 0 || playerHealth.currentHealth <= 0)
+        {
+            nav.enabled = false;
+        }
+        else if (enemyHealth.currentHealth <= fleeHealth && !hasFled)
+        {
+            if (!anim.GetBool( "isFleeing"))
+            { 
+                fleeTarget = this.transform.position - (this.transform.forward) * fleeDistance;
+                anim.SetBool( "isFleeing", true );
+                nav.enabled = true;
+            }
+            nav.SetDestination( fleeTarget );
+
+            if( Vector3.Distance( this.transform.position, fleeTarget ) <= 0.1f )
+            {
+                hasFled = true;
+                anim.SetBool( "isFleeing", false );
+            }
+        }
+        else if (Vector3.Distance(this.transform.position, player.transform.position) <= attackRange)
+        {
+            anim.SetBool("inAttackRange", true);
+            anim.SetBool("isPatrolling", false);
+            nav.enabled = false;
+        }                   
+        else if (Vector3.Distance(this.transform.position, player.transform.position) <= aggroRange)
+        {
+            anim.SetBool("inAggroRange", true);
+            anim.SetBool("inAttackRange", false);
+            nav.enabled = true;
+            nav.SetDestination(player.position);
+        }
+        else
+        {
+            anim.SetBool("inAggroRange", false);
+            Patrol();
+        }
+
+    }
+
+    void Patrol()
+    {
+        if (Vector3.Distance(this.transform.position, patrolPoints[currentPatrolPoint].position) <= 0.5f)
+        {
+            patrolIdleTimer += Time.deltaTime;
+
+            if (patrolIdleTimer < patrolIdleTime)
+            {
+                nav.enabled = false;
+                anim.SetBool("isPatrolling", false);
+            }
+            else
+            {
+                currentPatrolPoint = (currentPatrolPoint == patrolPoints.Length - 1) ? 0 : currentPatrolPoint + 1;
+                patrolIdleTimer = 0f;
+                nav.enabled = true;
+            }
+        }
+        else
+        {
+            nav.SetDestination(patrolPoints[currentPatrolPoint].position);
+            anim.SetBool("isPatrolling", true);
+        }
+
+    }
+}
+
+
---
+++ SevenWeapons/Assets/Scripts/Scripts (stephan old)/FireballMovement.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballMovement : MonoBehaviour {
+
+    public int damagePerShot = 50;
+    public float fireballSpeed = 10f;
+    public bool explosion = false;
+
+    PlayerShooting playerShooting;
+    GameObject player;
+
+    float timer;
+    Vector3 startFireballMarker;
+    Vector3 targetFireballTransform;
+    float fireballTravelDistance;
+    public float scaleFactor;
+
+    AudioSource fireballAudio;
+
+    int pauseBall = 0;
+    float pauseTimer = 0f;
+
+    void Awake()
+    {
+        playerShooting = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<PlayerShooting>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        fireballAudio = GetComponent<AudioSource>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        scaleFactor = this.transform.localScale.x;
+        this.transform.localScale += new Vector3(-scaleFactor, -scaleFactor, -scaleFactor);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("Fire1") && pauseBall == 0)
+        {
+            transform.position = playerShooting.transform.position;
+            pauseTimer += Time.deltaTime;
+            if (pauseTimer > 5f)
+                DestroyFireball();
+        }
+
+        if( Input.GetButtonUp( "Fire1" ) && pauseBall == 0 )
+        {
+            pauseBall = 1;
+            timer = 0f;
+            this.transform.localScale -= new Vector3(-scaleFactor, -scaleFactor, -scaleFactor);
+
+            if (pauseTimer < 2f)
+            {
+                int mod = (int)pauseTimer % 2;
+                fireballSpeed += (float)(mod / 2f) * fireballSpeed;
+            }
+            else
+                fireballSpeed += fireballSpeed;
+
+            fireballAudio.Play();
+
+            startFireballMarker = transform.position;
+            targetFireballTransform = playerShooting.transform.position + playerShooting.transform.forward * playerShooting.range;
+            fireballTravelDistance = Vector3.Distance( transform.position, targetFireballTransform );            
+
+        }
+        if( pauseBall == 1 && !explosion)
+        {
+            timer += Time.deltaTime ;
+
+            float distanceMoved = timer * fireballSpeed;
+            float fractionMoved = distanceMoved / fireballTravelDistance;
+            transform.position = Vector3.Lerp( startFireballMarker, targetFireballTransform, fractionMoved );
+            transform.Rotate(0f, -3f, 0f);
+            if (timer > 15f)
+                DestroyFireball();
+        }
+    }
+
+    public void DestroyFireball()
+    {
+        Destroy(gameObject, 0f);
+    }
+}
